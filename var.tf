0b9369e00439b7ab5dab3883f58fa21a09120a11
malar1995 <55018029+malar1995@users.noreply.github.com>
2023-02-18 19:05:55 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,7 +1,3 @@
-variable "region"{
-default = "ap-south-1"
-}
-
 variable "access_key"{
 default = "AKIAREO36Y2KBLW4ZA4M"
 }
@@ -10,11 +6,5 @@ variable "secret_key"{
 default = "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"
 }
 
-variable "ami"{
-default = "ami-0e742cca61fb65051"
-}
-
-variable "instance_type" {
-default = "t2.micro"
-}
-
+variable "region"{
+default = "us-east-2"  
