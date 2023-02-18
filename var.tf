a66545cd8e9a2e8a8090a241632ab001b7f198ce
malar1995 <55018029+malar1995@users.noreply.github.com>
2023-02-18 17:11:34 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,20 @@
+variable "region"{
+default = "ap-south-1"
+}
+
+variable "access_key"{
+default = "AKIAREO36Y2KBLW4ZA4M"
+}
+
+variable "secret_key"{
+default = "3yWz3kVaZsLn9zbYxFwmqSv2WpXra931GaVMfDTh"
+}
+
+variable "ami"{
+default = "ami-0e742cca61fb65051"
+}
+
+variable "instance_type" {
+default = "t2.micro"
+}
+
