f6010e7ac558517f144d5e765f065cb4ebebc98e
malar1995 <55018029+malar1995@users.noreply.github.com>
2023-02-18 20:32:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
   region = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
 }
