d318b1ae0f7148cd1dc203e8cc55839fc00e8f90
malar1995 <55018029+malar1995@users.noreply.github.com>
2023-02-18 19:04:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,10 +3,3 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-1111vfbeb"
-    key    = "terraform-state/tf.state"
-    region = "ap-south-1"
-  }
-}
