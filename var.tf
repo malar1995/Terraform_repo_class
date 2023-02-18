c37286ac0242f5b6923148b5974ee864d7751d71
malar1995 <55018029+malar1995@users.noreply.github.com>
2023-02-18 20:31:13 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,11 +1,3 @@
-variable "access_key"{
-default = "AKIAREO36Y2KBLW4ZA4M"
-}
-
-variable "secret_key"{
-default = "3yWz3kVaZsLn9zbYxFwmqSv2WpXra931GaVMfDTh"
-}
-
 variable "region"{
 default = "us-east-2"  
 }
